75308a5640ff4d8d0e6c056e44921b62edc73d6b
Jeremy Komarov <jeremy@boostapp.com>
2025-11-18 18:48:00 +0200

    SM-01-init-project

---
+++ src/main/java/com/firstProject/FirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.firstProject;
+
+import com.firstProject.controller.CustomerController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+}
